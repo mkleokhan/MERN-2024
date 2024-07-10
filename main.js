71865614dff9bd867a1bf5f2926f492b30277aed
mkleokhan <mkislamian@gmail.com>
2024-07-10 10:40:51 +0500

    created a new branch named function/palindrome and created a function name palindrome in it.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const palindrome=()=>{
+    console.log("Created a new branch named funtion/palindrome and created a new function in it...")
+}
+
+
 const main=()=>{
     console.log("main function to be pushed in to fucntion/main")
 }
